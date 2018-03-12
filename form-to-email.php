7e0fcdf7cbb2a8209046de568a1b9de0801f917a
Jonathan Yiv <JonathanYiv@gmail.com>
2018-03-12 12:46:30 -0400

    Add regex to prevent malicious injection

--- form-to-email.php
+++ form-to-email.php
@@ -1,4 +1,33 @@
 <?php
+  function IsInjected($str)
+  {
+    $injections = array('(\n+)',
+                        '(\r+)',
+                        '(\t+)',
+                        '(%0A+)',
+                        '(%0D+)',
+                        '(%08+)',
+                        '(%09+)'
+                       );
+
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+
+    if(preg_match($inject, $str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  if(IsInjected($email))
+  {
+    exit;
+  }
+
   $name = $_POST['name'];
   $company = $_POST['company'];
   $email = $_POST['email'];
@@ -12,5 +41,6 @@
   $to = "jonathanyiv@8bitbento.com, benjaminpetersen@8bitbento.com";
   $headers = "From: $email_from \r\n";
   $headers .= "Reply-To: $email \r\n";
+
   mail($to, $email_subject, $email_body, $headers);
 ?>
